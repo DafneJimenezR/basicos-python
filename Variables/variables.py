58b4b5eb599caa73dee967dbb30c2db1154f8090
DafneJimenezR <dafnejimrom@gmail.com>
2022-09-21 15:50:02 -0500

    Estructuras basicas

---
+++ Variables/variables.py
@@ -0,0 +1,18 @@
+x = 73
+y = 1 + x * 2 # variable con una operación matemática 
+z = ("hola a todos") # variable de caracteres 
+w = input ("ingresa tu color favorito") # variable de ingreso de caracteres 
+cantidad = int (input ("Ingresa tu edad")) # variable de ingreso numérico entero 
+peso = float (input ("¿cuánto pesas?")) # variable de ingreso de tipo decimal 
+
+x = 5 
+y = 1 + x*5 
+z = ("hola a todos") 
+w = input ("ingresa tu color favorito: ") 
+edad = int (input ("ingresa tu edad: ")) 
+peso = float (input ("¿cuánto pesas? "))  
+
+#imprime los datos 
+print("El valor de x es:", x) 
+print("El valor de y es:", y) 
+print("Tu color favorito es ",w, ", tienes",edad, " años y pesas ",peso," kg") 
